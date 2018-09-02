d9ba503a4d882c2130a1ae6369c9752c8d43d15b
Seaweeds <chawan.p.kc44@gmail.com>
2018-09-02 21:39:24 +0700

    reform Order DB

--- js/shop-order-take.js
+++ js/shop-order-take.js
@@ -11,10 +11,65 @@ firebase.initializeApp(config);
 
 // Get a reference to the database service
 var database = firebase.database();
+var orderRef = firebase.database().ref('orders');
 
+// --- CLICKED on button's ID: confirmOrderBtn ---
 $("#confirmOrderBtn").click(function(e) {
-    alert( "Handler for .submit() called." );
+
+	var currentDate = new Date();
+
+	var dateStamp = Math.floor(Date.now());
+	var date = currentDate.getDate();
+	var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+	var year = currentDate.getFullYear();
+	var hour = currentDate.getHours();
+	var minute = currentDate.getMinutes(); //Be careful! January is 0 not 1
+	var second = currentDate.getSeconds();
+
+	//var utcDate = new Date(Date.UTC(year, month, date-1, hour-7, minute, second));
+	var dateString = date + "-" +(month + 1) + "-" + year;
+	var timeString = hour + ":" +(minute)+ ":" + second;
+
+	var customerID = $("#customerID").val();
+	var tagID = $("#tagID").val();
+
+
+	//alert(dateString);
+
+	if(customerID == ""){
+		alert("โปรดระบุเบอร์โทรศัพท์ของ ลูกค้า");
+		return;
+	}else if(!parseInt(customerID)){
+		alert("โปรดระบุเบอร์โทรศัพท์ด้วย 0-9");
+		return;
+	}else if(tagID == ""){
+		alert("แสกน Tag ไม่สำเร็จ");
+		return;
+	}
+
+	var orderData = {
+		price: 0,
+		date: dateString,
+    	time: timeString,
+  	};
+
+  	var orderDetail = {
+  		total: 0
+  	};
+
+  	// Write the new post's data simultaneously in the posts list and the user's post list.
+  	var updateOrderData = {};
+  	var updateOrderDetail = {};
+
+  	updateOrderData['/orders/' + customerID + '/' + tagID] = orderData;
+  	firebase.database().ref().update(updateOrderData);
+
+ 	updateOrderDetail['/orders/' + customerID + '/' + tagID + '/detail'] = orderDetail;
+ 	firebase.database().ref().update(updateOrderDetail);
   
+
+    alert( "Success" );
+  	location.href = "shop-order-take-success.html";
 });
 
 // Call UID of user
@@ -23,7 +78,7 @@ firebase.auth().onAuthStateChanged(function(user) {
  	alert(user.uid);
 });
 
-var orderRef = firebase.database().ref('items');
+
 
 orderRef.on('child_added', function(data) {
   alert(data.key);
